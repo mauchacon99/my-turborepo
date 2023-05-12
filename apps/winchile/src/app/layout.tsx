857cd36423d748e87fcc0558beb74bd0ec3575cd
mauchaconrithmXO <Mauricio.Chacon@rithmXO.com>
2023-05-12 10:25:17 -0400

    fix

--- apps/winchile/src/app/layout.tsx
+++ apps/winchile/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 // include styles from the ui package
-import "ui/styles.css";
+import "ui-winchile/styles.css";
 
 export default function RootLayout({
   children,
